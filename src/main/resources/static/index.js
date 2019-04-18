59285d39e4cd6a9c8f2d97708e0a65ac073473ac
Tsai Bo Wei(Brian) <eliot9010@gmail.com>
2019-04-18 15:04:35 +0800

    done basic vue course

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,6 +2,16 @@
  * Learning to use Vue.js
  */
 
+/* components */
+Vue.component( 
+	'todo-item',
+	{
+		props:['todo'],
+		template:'<li>{{todo.text}}</li>'
+	}
+)
+
+
 var app = new Vue({ 
     el: '#app',
     data: {
@@ -11,8 +21,21 @@ var app = new Vue({
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
-        ]
+        ],
+        /* for todo-item components */
+		groceryList: [
+		    { id: 0, text: 'Vegetables' },
+		    { id: 1, text: 'Cheese' },
+		    { id: 2, text: 'Whatever else humans are supposed to eat' }
+		]
+    },
+    methods:{
+    	reverseMessage: function(){
+    		this.message = this.message.split('').reverse().join('')
+    	}
     }
+
+
 });
 
 app.message = 'I have changed the data!';
